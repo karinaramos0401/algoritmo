f64eee86e010dbc97805089ac4e4e06177f93bf0
karinaramos0401 <84257110+karinaramos0401@users.noreply.github.com>
2021-11-12 08:12:39 -0300

    changes year 14

--- human-resource-machine/year14.js
+++ human-resource-machine/year14.js
@@ -1,16 +1,12 @@
 function year14 (num1,num2) { 
-  if (num1 > num2) {
-      console.log(num1)
-
-  } else 
-        if (num2 > num1) {
-      console.log(num2)
+     if (num2 > num1) {
+        console.log(num2)
 
   }
-    else {
+     else {
   console.log(num1)
       
-    }
+  }
 }
 
 year14(8,5)
