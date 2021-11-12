62a3a4f75707827dee46df371827d422b0f9a79e
karinaramos0401 <84257110+karinaramos0401@users.noreply.github.com>
2021-11-12 07:46:25 -0300

    year 14

---
+++ human-resource-machine/year14.js
@@ -0,0 +1,18 @@
+function year14 (num1,num2) { 
+  if (num1 > num2) {
+      console.log(num1)
+
+  } else 
+        if (num2 > num1) {
+      console.log(num2)
+
+  }
+    else {
+  console.log(num1)
+      
+    }
+}
+
+year14(8,5)
+year14(5,9)
+year14(5,5)
