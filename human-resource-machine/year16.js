a0c23f00b516b623378a578d577bb4dd44c56eb8
karinaramos0401 <84257110+karinaramos0401@users.noreply.github.com>
2021-11-16 07:25:32 -0300

    year 16

---
+++ human-resource-machine/year16.js
@@ -0,0 +1,13 @@
+function year16 (num) {
+    if(num <0) {
+        let negative = num *(-1); 
+        console.log (negative);
+    } else {
+        let positive = num;
+        console.log(positive); 
+    }
+        
+}
+
+year16(-10);
+year16(2);
