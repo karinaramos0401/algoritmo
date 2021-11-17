5c32dbe3c5acdf5b91796917ed05a85c81265d9f
karinaramos0401 <84257110+karinaramos0401@users.noreply.github.com>
2021-11-17 07:25:54 -0300

    year 17

---
+++ human-resource-machine/year17.js
@@ -0,0 +1,11 @@
+function year16 (num1,num2) {
+    if(num1 <=0  & num2 <=0 || num1 >= 0 & num2>=0 ) {
+        console.log(0);
+    } else {
+        console.log(1);
+    }
+}
+
+year16(-1,-1);
+year16(2,5);
+year16(-1,2);
