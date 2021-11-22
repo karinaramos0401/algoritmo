3f25544b660f2b2b9d0b8694e77e313f0a35fdc2
karinaramos0401 <84257110+karinaramos0401@users.noreply.github.com>
2021-11-22 09:22:51 -0300

    year 21

---
+++ human-resource-machine/year21.js
@@ -0,0 +1,22 @@
+// const year21 = (num) => {
+//    const total = 0;
+//    for (let i =0; 1 <= num; i++) 
+//    total = total + numero[i]
+// }
+
+// return total
+
+// console.log(year21([1,2,3]))
+
+
+const year21 = (num) => {
+  let total = 0;
+
+  numeros.foreach((numero) => {
+    total = total + numero
+  })
+
+  return total
+}
+
+console.log(year4([1,2,3,4,5]))
