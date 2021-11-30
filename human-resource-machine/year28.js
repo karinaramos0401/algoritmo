e25d80fb0e33adb4d54aaeaa6e9a6a6081e5420f
karinaramos0401 <84257110+karinaramos0401@users.noreply.github.com>
2021-11-30 07:24:52 -0300

    year 28 and year 29

---
+++ human-resource-machine/year28.js
@@ -0,0 +1,6 @@
+const year28 = (numeros) => { 
+    numeros.sort((a,b) =>  a-b);
+        return numeros
+}
+
+console.log(year28([100,25,3]))
---
+++ human-resource-machine/year29.js
@@ -0,0 +1,7 @@
+const year29 = (endereço) => { 
+  const piso = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+    return piso[endereço]
+}
+
+console.log(year29(0)); 
+console.log(year29(4));
